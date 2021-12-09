3ca7b0dcc30117f88d69f4ea57ff9babc314170c
风船野 <18392939411@163.com>
2021-12-09 11:36:00 +0800

    清除

--- 00001.py
+++
@@ -1,3 +0,0 @@
-from pyecharts.charts import Page
-
-Page.save_resize_html(r'E:\Jupyter Notebook\Data Test\疫情可视化.html',cfg_file = r'E:\Jupyter Notebook\Data Test\chart_config.json',dest='疫情实时追踪平台.html')
